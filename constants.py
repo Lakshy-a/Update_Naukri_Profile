55a8708af3610cbb64d721cbf0898b9eca876ba1
Lakshya Gupta <lakshya.gupta@adglobal360.in>
2025-08-05 13:06:40 +0530

    Added naukri script and dependencies

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # Configuration and constants used in the script. Update values as needed.
 
-USERNAME = "your_naukri_username"
-PASSWORD = "your_naukri_password"
-MOBILE = "your_naukri_phone"
-ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
-MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
+USERNAME = "lakshyagupta8786@gmail.com"
+PASSWORD = "12345678La@"
+MOBILE = "8954228484"
+ORIGINAL_RESUME_PATH = r"C:\Users\admin\Desktop\Lakshya_Gupta_Resume_2025.pdf"
+MODIFIED_RESUME_PATH=r"C:\Users\\admin\Desktop\Lakshya_Gupta_Resume_2025.pdf"
 NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
 NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
